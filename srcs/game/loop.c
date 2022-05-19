3a6e552574d26b2690b040d38033b898cf6b4528
Celine DINE <cdine@e3r1p13.42.fr>
2022-05-19 15:56:04 +0200

    event button press

--- srcs/game/loop.c
+++ srcs/game/loop.c
@@ -6,7 +6,7 @@
 /*   By: cdine <cdine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 15:26:39 by ntan              #+#    #+#             */
-/*   Updated: 2022/05/19 11:48:36 by cdine            ###   ########.fr       */
+/*   Updated: 2022/05/19 15:55:46 by cdine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -171,8 +171,9 @@ int	game_loop(t_prog *cub3d)
 	cub3d->minimap_int = create_sprite(cub3d, cub3d->mlx, "srcs/game/12.xpm");
 	init_window(cub3d, 1280, 720, "cub3d");
 	mlx_do_key_autorepeaton(cub3d->mlx);
-	mlx_hook(cub3d->win, 17, 0 , ending, cub3d);
+	mlx_hook(cub3d->win, 17, 0, ending, cub3d);
 	mlx_key_hook(cub3d->win, input, cub3d);
+	mlx_hook(cub3d->win, 2, (1L << 0), input, cub3d);
 	mlx_loop_hook(cub3d->mlx, update, cub3d);
 	mlx_loop(cub3d->mlx);
 	return (0);
